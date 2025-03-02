6e4738e8fa7f9329a1d2a92918108d27ec9c7c19
ammadhamid <ammadhamid721@gmail.com>
2025-03-02 19:45:38 +0500

    Created SearchForm Component as SSR Component which has SearchReset Componetn as CSR Component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import SearchForm from "../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
-    <>
-      <h1 className="heading">Home</h1>;
-    </>
+    <div className="pink-container">
+      <h1 className="heading">
+        Pitch Your Startup <br />
+        Connect with Entrepreneurs
+      </h1>
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas, Vote on Pitches, and Get noticed in Virtual Competetion
+      </p>
+      <SearchForm query={query} />
+    </div>
   );
 }
